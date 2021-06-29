b5568c79e7ca885cb97d1f3932b8602ab96df429
Lucy <32479481+lucyel@users.noreply.github.com>
2021-06-29 14:35:48 +0700

    Merge all code to main
    merge hash function to main and refactor some char.
    Add check hash change for file.

--- ids-rule.py
+++ ids-rule.py
@@ -1,40 +1,70 @@
-# TODO: delete the file with rule that has no ip iocs
 # TODO: compare 2 iocs file to check if it is skipable
 # TODO: download folder not from git
+# TODO: check if donwloaded file is type acsii text, if is other type then not download.
+# TODO: convert other iocs format: IP:PORT [done]
 
 import re
 import base64
 import subprocess
-from hash_gen import gen_hash_from_file
+import os
+import hashlib
 from os import listdir
 from os.path import isfile, join
 
+# Input : file path needed to hash
+def gen_hash_from_file(filepath):
+    BUF_SIZE = 65536
+    sha256_hash = hashlib.sha256()
+    with open(filepath, "rb") as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha256_hash.update(data)
+    return sha256_hash.hexdigest()
 
-def iocs_to_ids_rules(filename, mystr):
-    for i in range(0, len(filename)):
-        my_file = open(f"D:\\Downloads\\test\\{filename[i]}", "r")
-        domain_encoded = open(f"D:\\Downloads\\encoded_{filename[i]}", "w")
+
+def download_from_git(git_url, return_path):
+    print("do something")
+
+
+def download_from_other(url, return_path):
+    print("do something")
+
+
+# Convert ip to rule, output base64 code of domain name to a file, create the rule contain ip rule and a dns rule.
+# Input : a list type of file name, and the string that contain the first char of the rule for IP type.
+def iocs_to_ids_rules(list_file_name):
+    rule_str = "["
+    for i in range(0, len(list_file_name)):
+        my_file = open(f"D:\\Downloads\\test\\{list_file_name[i]}", "r")
+        domain_encoded = open(f"D:\\Downloads\\res\\encoded_{list_file_name[i]}", "w")
         my_line = my_file.readlines()
         for j in range(0, len(my_line)):
-            if (bool(re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", my_line[j]))) and (
-            not re.match(r"^#", my_line[j])):
+            if (bool(re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+$", my_line[j]))) and (not re.match(r"^#", my_line[j])):
+                res = re.split(r":", my_line[j])
+                for k in range(0, 1):
+                    ip_data = res[0]
+                    port_data = res[1]
+                    # print(f"{ip_data} and {port_data}")
+            if (bool(re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", my_line[j]))) and (not re.match(r"^#", my_line[j])):
                 my_line[j] = str.rstrip(my_line[j])
-                mystr += my_line[j]
-                mystr += ","
+                rule_str += my_line[j]
+                rule_str += ","
             elif (bool(re.match(r".*\..*", my_line[j]))) and (not re.match(r"^#", my_line[j])):
                 message_bytes = my_line[j].encode('ascii')
                 base64_bytes = base64.b64encode(message_bytes)
                 base64_message = base64_bytes.decode('ascii')
                 print(base64_message, file=domain_encoded)
-        mystr = mystr[:-1]
-        mystr += "]"
-        outfile = open(f"D:\\Downloads\\{filename[i]}.rules", "w")
+        rule_str = rule_str[:-1]
+        rule_str += "]"
+        outfile = open(f"D:\\Downloads\\res\\{list_file_name[i]}.rules", "w")
         print(
-            f"alert any any -> any any (msg:\"ET DNS query for {filename[i]}\"; reference:url,https://github.com/stamparm/maltrail/blob/master/README.md; dns.query; dataset:set, {filename[i]}, type string, load: {filename[i]}.lst; sid:202025113; rev:1;))",
+            f"alert any any -> any any (msg:\"ET DNS query for {list_file_name[i]}\"; reference:url,https://github.com/stamparm/maltrail/blob/master/README.md; dns.query; dataset:set, {list_file_name[i]}, type string, load: {list_file_name[i]}.lst; sid:202025113; rev:1;))",
             file=outfile)
-        if not bool(re.match(r"^]$", mystr)):
-            print(f"alert {mystr} any -> any any (msg:\"something\")", file=outfile)
-        mystr = "["
+        if not bool(re.match(r"^]$", rule_str)):
+            print(f"alert {rule_str} any -> any any (msg:\"something\")", file=outfile)
+        rule_str = "["
     my_file.close()
     domain_encoded.close()
 
@@ -46,7 +76,7 @@ def iocs_to_siem_rules():
 # Run only in linux
 def add_to_dataset(filename):
     for i in range(0, len(filename)):
-        domain_encoded = open(f"D:\\Downloads\\encoded_{filename[i]}", "r")
+        domain_encoded = open(f"D:\\Downloads\\res\\encoded_{filename[i]}", "r")
         encode_dns = domain_encoded.readlines()
         number_of_elements = len(encode_dns)
         for j in range(0, number_of_elements):
@@ -55,9 +85,33 @@ def add_to_dataset(filename):
             output, error = process.communicate()
 
 
-my_str = "["
+
 folder_path = r"D:\Downloads\test"
 file_name = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
 
 
-iocs_to_ids_rules(file_name, my_str)
+hash_file_path_new = open(r"D:\\Downloads\\res\\hash_file_new.txt", "w")
+for i in range(0, len(file_name)):
+    gen_hash = gen_hash_from_file(f"D:\\Downloads\\test\\{file_name[i]}")
+    print(f"{file_name[i]} : {gen_hash}", file=hash_file_path_new)
+
+if not os.path.isfile(r"D:\\Downloads\\res\\hash_file.txt"):
+    hash_file_path = open(r"D:\\Downloads\\res\\hash_file.txt", "w")
+    for i in range(0, len(file_name)):
+        gen_hash = gen_hash_from_file(f"D:\\Downloads\\test\\{file_name[i]}")
+        print(f"{file_name[i]} : {gen_hash}", file=hash_file_path)
+    #iocs_to_ids_rules(file_name)
+else:
+    my_res = list()
+    hash_file_path = open(r"D:\\Downloads\\res\\hash_file.txt", "r")
+    hash_file_path_new = open(r"D:\\Downloads\\res\\hash_file_new.txt", "r")
+    my_line = hash_file_path.readlines()
+    my_line_new = hash_file_path_new.readlines()
+    for i in range(0, len(my_line)):
+        my_line[i] = str.strip(my_line[i])
+        my_line_new[i] = str.strip(my_line_new[i])
+        if my_line[i] != my_line_new[i]:
+            res = re.split("\s:\s", my_line[i])
+            my_res.append(res[0])
+    print(my_res)
+    #iocs_to_ids_rules(my_res)
