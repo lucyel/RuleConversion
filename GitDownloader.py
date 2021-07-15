7ed89203f52866faa9a09e9d78d35fd46e7427c0
Lucy <32479481+lucyel@users.noreply.github.com>
2021-07-15 18:59:45 +0700

    fix error with git folder that has multi path

--- GitDownloader.py
+++ GitDownloader.py
@@ -3,12 +3,13 @@ import os
 import urllib.request
 import json
 
+
 def create_url(url):
-    repo_only_url = re.compile(r"https:\/\/github\.com\/[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}\/[a-zA-Z0-9]+")
     re_branch = re.compile("/(tree|blob)/(.+?)/")
     branch = re_branch.search(url)
     download_dirs = url[branch.end():]
-    api_url = (url[:branch.start()].replace("github.com", "api.github.com/repos", 1) + "/contents/" + download_dirs + "?ref=" + branch.group(2))
+    api_url = (url[:branch.start()].replace("github.com", "api.github.com/repos", 1) + "/contents/" + download_dirs +
+               "?ref=" + branch.group(2))
     return api_url, download_dirs
 
 
@@ -23,16 +24,11 @@ def download(repo_url, output_dir, flatten=False,):
     else:
         dir_out = output_dir
 
-
-    opener = urllib.request.build_opener()
+    proxy = urllib.request.ProxyHandler({'http': '10.1.7.56:8080', 'https': '10.1.7.56:8080'})
+    opener = urllib.request.build_opener(proxy)
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
     response = urllib.request.urlretrieve(api_url)
-
-    if not flatten:
-        # make a directory with the name which is taken from
-        # the actual repo
-        os.makedirs(dir_out, exist_ok=True)
 
     total_files = 0
 
@@ -42,35 +38,24 @@ def download(repo_url, output_dir, flatten=False,):
         # can use it for the output information later
         total_files += len(data)
 
-        # If the data is a file, download it as one.
-        if isinstance(data, dict) and data["type"] == "file":
-            # download the file
-            opener = urllib.request.build_opener()
-            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-            urllib.request.install_opener(opener)
-            urllib.request.urlretrieve(data["download_url"], os.path.join(dir_out, data["name"]))
-            return total_files
-
         for file in data:
             file_url = file["download_url"]
-            file_name = file["name"]
 
             if flatten:
                 path = os.path.basename(file["path"])
             else:
                 path = file["path"]
             dirname = os.path.dirname(path)
+            print(dirname)
+            path_1 = path.split("/")
 
-            if dirname != '':
-                os.makedirs(os.path.dirname(path), exist_ok=True)
-            else:
-                pass
+            path2 = f"{path_1[-2]}/{path_1[-1]}"
 
-            os.makedirs(f"{output_dir}\{dirname}", exist_ok=True)
+            os.makedirs(fr"{output_dir}/{path_1[-2]}", exist_ok=True)
 
             if file_url is not None:
-                download_path = f"{output_dir}\{path}"
-                opener = urllib.request.build_opener()
+                download_path = fr"{output_dir}/{path2}"
+                opener = urllib.request.build_opener(proxy)
                 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
                 urllib.request.install_opener(opener)
                 # download the file
@@ -78,3 +63,5 @@ def download(repo_url, output_dir, flatten=False,):
             else:
                 download(file["html_url"], flatten, dir_out)
     return total_files
+
+#download("https://github.com/Neo23x0/signature-base/tree/master/iocs", "D:\Downloads\download_test", False)
